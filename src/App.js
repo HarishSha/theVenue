060a50a0eb0a71a2560578ef2ba5bc17a96836b6
Grow <grow@Harishs-MacBook-Air.local>
2018-11-23 17:34:19 +0530

    adding images and carrousel

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import './resources/styles.css';
 
-import Header from './components/header_footer/Header'
+import Header from './components/header_footer/Header';
+import Featured from './components/featured/index';
 
 
 
@@ -11,6 +12,7 @@ class App extends Component {
     return (
       <div className="App" style={{ height:'1500px', background:'cornflowerblue ' }}>
         <Header /> 
+        <Featured />
       </div>
     );
   }
